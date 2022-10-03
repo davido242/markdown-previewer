57d4ffc0b76625dbbe95cccdf2550b6258a576dd
Monday_David_Sarka <david_md@hamari.agency>
2022-10-03 17:59:06 +0100

    added component and text area with its functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-function App() {
+import { useState } from "react";
+
+const Editor = ({ content, handleTextareaChange }) => {
   return (
-    <div className="App">
-    <h2>Hi Daveed!</h2>
+    <textarea value={content} onChange={handleTextareaChange}/>
+  )
+};
+
+function App() {
+  const [content, setContent] = useState("Say She no want Daveedo, she wanty my love o");
+  const handleTextareaChange = (sev) => {
+    setContent(sev.target.value)
+  }
+  
+  return (
+    <div className="main">
+      < Editor content={content} handleTextareaChange={handleTextareaChange} />
     </div>
   );
 }
