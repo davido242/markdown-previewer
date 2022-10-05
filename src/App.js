57c2435e0da73dd1c93c0313ba1d083eb3ebef7b
Monday_David_Sarka <david_md@hamari.agency>
2022-10-05 11:32:07 +0100

    Fixed a bug with multi line of code within bactics

--- src/App.js
+++ src/App.js
@@ -1,36 +1,35 @@
 import { useState } from "react";
-import { marked } from 'marked';
+import { marked } from "marked";
 // import { Prism } from "prismjs";
-import { highlight, languages } from 'prismjs/components/prism-core';
+import { highlight, languages } from "prismjs/components/prism-core";
 // import { render } from "@testing-library/react";
-
 
 marked.setOptions({
   break: true,
   highlight: function (code) {
-    return highlight(code, languages.javascript, 'javascript');
-  }
+    return highlight(code, languages.javascript, "javascript");
+  },
 });
 
 const renderer = new marked.Renderer();
-renderer.link = function (href, title, text){
+renderer.link = function (href, title, text) {
   return `<a target="_blank" href="${href}">${text}</a>`;
 };
 
 const Editor = ({ content, handleTextareaChange }) => {
-  return (
-    <textarea value={content} onChange={handleTextareaChange}/>
-  )
+  return <textarea value={content} onChange={handleTextareaChange} />;
 };
 
 const Previewer = ({ content }) => {
   return (
-    <div id="preview" dangerouslySetInnerHTML={{
-        __html: marked(content, { renderer: renderer})
+    <div
+      id="preview"
+      dangerouslySetInnerHTML={{
+        __html: marked(content, { renderer: renderer }),
       }}
     />
-  )
-}
+  );
+};
 
 function App() {
   const [content, setContent] = useState(`
@@ -38,13 +37,14 @@ function App() {
   ## This is H2
   [title](https://www.example.com)
   \`code\`
+
   \`\`\`
-    {
-      firstName: "John",
-      "lastName": "Smith",
-      "age": 25
-    }
-  \`\`\`
+      {
+        "firstName": "John",
+        "lastName": "Smith",
+        "age": 25
+      }
+    \`\`\`
 
   - First Item
   - Second Item
@@ -53,16 +53,17 @@ function App() {
   >blockquote
 
   ![alt text](https://image.shutterstock.com/image-photo/sexy-body-young-sporty-healthy-600w-678312892.jpg)
+
   **Bold Text**
   `);
   const handleTextareaChange = (sev) => {
-    setContent(sev.target.value)
-  }
-  
+    setContent(sev.target.value);
+  };
+
   return (
     <div className="main">
-      < Editor content={content} handleTextareaChange={handleTextareaChange} />
-      <Previewer  content={content}/>
+      <Editor content={content} handleTextareaChange={handleTextareaChange} />
+      <Previewer content={content} />
     </div>
   );
 }
